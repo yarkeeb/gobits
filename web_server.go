ffa284e4152ac3647a9a56cae45dd1019479ff5b
Yaroslav Romanov <robot.xet@gmail.com>
2015-10-26 23:05:56 +0300

    Add simple web server on Go

---
+++ web_server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form) // parse arguments, you have to call this by yourself
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("val: ", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello, robotzet!") //send data to client side
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)       // set router
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
