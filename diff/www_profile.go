f452e923dbfe8a62d8436713110923476b91fed3
Yaroslav Romanov <robot.xet@gmail.com>
2020-08-21 15:26:24 +0300

    stuff

---
+++ diff/www_profile.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/pprof"
+	"time"
+)
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Serving; %s\n", r.URL.Path)
+	fmt.Printf("Served: %s\n", r.Host)
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().Format(time.RFC1123)
+	Body := "The current time is: "
+	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", Body)
+	fmt.Fprintf(w, "<h2 align=\"center\">%s</h2>", t)
+	fmt.Fprintf(w, "Serving; %s\n", r.URL.Path)
+	fmt.Printf("Served time for: %s\n", r.Host)
+}
+
+func main() {
+	PORT := ":8001"
+
+	r := http.NewServeMux()
+	r.HandleFunc("/time", timeHandler)
+	r.HandleFunc("/", myHandler)
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	err := http.ListenAndServe(PORT, r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
